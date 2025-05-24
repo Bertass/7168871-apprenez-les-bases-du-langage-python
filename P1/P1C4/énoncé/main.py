cdf103455e41add7bf7c053a7f801793b750cc31
Bertass <154248128+Bertass@users.noreply.github.com>
2025-05-24 17:40:34 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+# Créez une variable nommée  nom  et attribuez-y votre nom.
+nom = "FAYE"
+# Créez une variable nommée  age  et attribuez-y votre âge.
+age = 37
+# Affichez une phrase contenant votre nom et votre âge en utilisant les variables que vous venez de créer.
+print ("Bonjour, je m'appelle {nom} et j'ai {age} ans ") 
